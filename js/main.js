b95ab2b78eb8b3a2ae839a5b0e5721ed340fe9e4
Gabriela Benejam <gbenejam@Gabrielas-Air.home>
2017-08-19 15:43:42 +0200

    Basic gameplay implemented

--- js/main.js
+++ js/main.js
@@ -41,44 +41,49 @@ function create() {
   }, this);
   //Adding bar and its physics
   bar = game.add.sprite(game.world.width * 0.5, game.world.height - 5, 'bar');
+  game.physics.enable(bar, Phaser.Physics.ARCADE);
   bar.anchor.set(1, 1);
   bar.body.collideWorldBounds = true;
-  game.physics.enable(bar, Phaser.Physics.ARCADE);
   bar.body.immovable = true;
-  initBricks();
 
+  initBricks();
 }
 
 function update() {
   //Setting collisions
   game.physics.arcade.collide(ball, bar);
+  game.physics.arcade.collide(ball, bricks, ballHitBrick);
   bar.x = game.input.x || game.world.width * 0.5;
 }
 
+function ballHitBrick(ball,brick) {
+  brick.kill();
+}
+
 function initBricks() {
-  brickInfo = {
-    width: 50,
-    height: 20,
-    count: {
-      row: 7,
-      col: 3
-    },
-    offset: {
-      top: 50,
-      left: 60
-    },
-    padding: 10
-  }
-  bricks = game.add.group();
-  for (c = 0; c < brickInfo.count.col; c++) {
-    for (r = 0; r < brickInfo.count.row; r++) {
-      var brickX = (r * (brickInfo.width + brickInfo.padding)) + brickInfo.offset.left;
-      var brickY = (c * (brickInfo.height + brickInfo.padding)) + brickInfo.offset.top;
-      newBrick = game.add.sprite(brickX, brickY, 'brick');
-      game.physics.enable(newBrick, Phaser.Physics.ARCADE);
-      newBrick.body.immovable = true;
-      newBrick.anchor.set(0.5);
-      bricks.add(newBrick);
+    brickInfo = {
+        width: 50,
+        height: 20,
+        count: {
+            row: 7,
+            col: 3
+        },
+        offset: {
+            top: 50,
+            left: 60
+        },
+        padding: 10
     }
-  }
+    bricks = game.add.group();
+    for(c=0; c<brickInfo.count.col; c++) {
+        for(r=0; r<brickInfo.count.row; r++) {
+            var brickX = (r*(brickInfo.width+brickInfo.padding))+brickInfo.offset.left;
+            var brickY = (c*(brickInfo.height+brickInfo.padding))+brickInfo.offset.top;
+            newBrick = game.add.sprite(brickX, brickY, 'brick');
+            game.physics.enable(newBrick, Phaser.Physics.ARCADE);
+            newBrick.body.immovable = true;
+            newBrick.anchor.set(0.5);
+            bricks.add(newBrick);
+        }
+    }
 }
